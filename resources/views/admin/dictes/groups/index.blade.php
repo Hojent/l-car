a9fb93ff22addc590ab0efb50c2032131aed296f
Hojent <irin_german@yahoo.com>
2020-07-25 16:04:07 +0300

    Number of Parts in the common group list

--- resources/views/admin/dictes/groups/index.blade.php
+++ resources/views/admin/dictes/groups/index.blade.php
@@ -16,7 +16,7 @@
                     <li class="d-flex no-block card-body border-top">
                         <i class="fa fa-check-circle w-30px m-t-5"></i>
                         <div>
-                            <a href="{{route('groups.edit',$group->id)}}" class="m-b-0 font-medium p-0" title="@lang('headers.edit_group')">{{$group->group}}</a>
+                            <a href="{{route('groups.edit',$group->id)}}" class="m-b-0 font-medium p-0" title="@lang('headers.edit_group')">{{$group->group}} ({{$group->parts->count()}}) </a>
                         </div>
                         <div class="ml-auto">
                             {{Form::open(['route'=>['groups.destroy', $group->id], 'method'=>'delete'])}}
